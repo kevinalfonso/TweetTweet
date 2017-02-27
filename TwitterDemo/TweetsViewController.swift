99aa498097480dfd0784e0ab63e2c049a55fb27d
Kevin Alfonso <kevin@kevins-mbp.fios-router.home>
2017-02-27 00:06:36 -0500

    commit

--- TwitterDemo/TweetsViewController.swift
+++ TwitterDemo/TweetsViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class TweetsViewController: UIViewController {
+class TweetsViewController: UIViewController//, UITableViewDataSource, UITableViewDelegate,
+{
 
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     var tweets: [Tweet]!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +27,9 @@ class TweetsViewController: UIViewController {
         }) { (error) in
             print(error.localizedDescription)
         }
+        
+//        tableView.dataSource = self
+//        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,6 +40,19 @@ class TweetsViewController: UIViewController {
     @IBAction func onLogoutButton(_ sender: UIBarButtonItem) {
         TwitterClient.sharedInstance.logout()
     }
+    
+//    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return tweets.count
+//        
+//    }
+//    
+//    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell") as! TweetCell
+//        cell.tweet = tweets[indexPath.row]
+//        
+//        return cell
+//    }
+    
 
     /*
     // MARK: - Navigation
